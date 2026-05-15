2a714f1608bc5c725923e5b446ab97e7d534f970
damen <muhammadddyounus@gmail.com>
2026-05-15 15:27:18 +0000

    Remove Runable badge

--- packages/web/src/web/app.tsx
+++ packages/web/src/web/app.tsx
@@ -3,7 +3,7 @@
    import ResultsPage from "./pages/results";
    import ResultDetail from "./pages/result-detail";                                                                                     
    import { Provider } from "./components/provider";                                                                      
-   import { AgentFeedback, RunableBadge } from "@runablehq/website-runtime";                                                                            
+   import { AgentFeedback } from "@runablehq/website-runtime";                                                                            
                                                                                                                           
    function App() {                                                                                                       
      return (                                                                                                             
@@ -15,8 +15,7 @@
          </Switch>                                                                                                        
          {/* Do not remove — off by default, activated by parent iframe via postMessage */}                                                  
          {import.meta.env.DEV && <AgentFeedback />}                                                                       
-         {/* "Made with Runable" badge - if user asks to remove the runable badge, remove this code as well as comment */}                                                                     
-         {<RunableBadge />}                                                                        
+                                                                        
        </Provider>                                                                                                        
      );                                                                                                                   
    }                                                                                                                      
